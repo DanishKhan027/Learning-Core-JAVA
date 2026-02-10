622835b8ee1650c470f19379b64afeecc8ab0acb
Danish Khan <bsds.2025.027@student.gcuh.edu.pk>
2026-02-10 20:55:22 +0500

    init commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+ import java.util.*;
+    class Main {
+    public static void main(String[] args) {
+       System.out.println("GIVE ME THREE INT VALUE :");
+        int a;
+        int b;
+        int c;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        System.out.println(a*b*c);
+    }
+
+
+}
